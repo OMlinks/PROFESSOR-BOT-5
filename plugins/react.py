d1da9a384536ad26cc89f7aba07b9e5b998dd1dd
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-05-10 23:43:09 +0530

    Update react.py

--- plugins/react.py
+++ plugins/react.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid, ReactionInvalid 
 from random import choice
 
-@TelegramBot.on_message(filters.all)
+@Client.on_message(filters.all)
 async def send_reaction(_, msg: Message):
     try:
         await msg.react(choice(Telegram.EMOJIS))
