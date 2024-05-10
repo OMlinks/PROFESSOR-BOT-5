89841e48f355cc5e8ffa91595bb1c2369d14a27a
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-05-11 00:11:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,6 +13,13 @@ from database.connections_mdb import active_connection
 logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 
+@Client.on_message(filters.all)
+async def send_reaction(client, message: Message):
+    try:
+        await message.react(emoji=random.choice(DS_REACT))
+        return
+
+
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
