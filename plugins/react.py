7fac87552709462ea9c466b66b0b2f70c8d75b26
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-05-10 23:44:49 +0530

    Update react.py

--- plugins/react.py
+++ plugins/react.py
@@ -2,11 +2,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid, ReactionInvalid 
 from random import choice
+from info import DS_REACT
 
 @Client.on_message(filters.all)
-async def send_reaction(_, msg: Message):
+async def send_reaction(_, message: Message):
     try:
-        await msg.react(choice(Telegram.EMOJIS))
+        await message.react(emoji=random.choice(DS_REACT)
     except (
         MessageIdInvalid,
         EmoticonInvalid,
