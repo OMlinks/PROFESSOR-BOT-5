5f70de51e5484b115b662e7b96f8f7fec4e63e0d
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-05-10 23:41:15 +0530

    Update react.py

--- plugins/react.py
+++ plugins/react.py
@@ -2,8 +2,6 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid, ReactionInvalid 
 from random import choice
-from bot import TelegramBot
-from bot.config import Telegram
 
 @TelegramBot.on_message(filters.all)
 async def send_reaction(_, msg: Message):
